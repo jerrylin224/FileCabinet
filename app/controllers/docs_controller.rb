a40734f937716ffae24602a6225b430ed74fc84b
Mata Angelou <pk60905@hotmail.com>
2017-04-07 14:53:51 +0800

    Add docs views and define actinos

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,19 +1,26 @@
 class DocsController < ApplicationController
+  before_action :find_doc, only: [:show, :edit, :updeate, :destroy]
 
   def index
-    
+    @docs  = Doc.all.order("created_at DESC")
   end
 
   def show
-    
+    find_doc
   end
 
   def new
-    
+    @doc = Doc.new
   end
 
   def create
-    
+    @doc = Doc.new(doc_params)
+
+    if @doc.save
+      redirect_to docs_path
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -31,10 +38,11 @@ class DocsController < ApplicationController
   private
 
     def find_doc
-      
+      # @doc = Doc.find(id: doc_params[:id])      
+      @doc = Doc.find(params[:id])
     end
 
     def doc_params
-      
+      params.require(:doc).permit(:title, :content)
     end
 end
